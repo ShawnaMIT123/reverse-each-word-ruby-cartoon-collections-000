489f4150f7e3382e69ef444d50c0506e620c8bee
Shawna Davis <shawna_davis@alum.mit.edu>
2018-08-28 00:34:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(string)
   newstring  = string.split(" ")
+<<<<<<< HEAD
   array = newstring.collect do |word| 
      word.reverse
   end
 array.join(" ")
+=======
+  newstring.collect do |word| 
+     word.reverse
+  end
+
+>>>>>>> 8d1918bcfc442584aac405c4650434989d349cf0
 end
